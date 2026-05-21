8fb4e3dfcb65cca4f6621150f831b561056cd804
Pinky <pinkysingh.sde@gmail.com>
2026-05-21 11:29:51 +0530

    url fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+   baseURL:
+    process.env.NEXT_PUBLIC_API_URL +
+    "/api",
 });
 
 export default API;
--- src/services/socket.ts
+++ src/services/socket.ts
@@ -2,6 +2,9 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(
+    // "http://localhost:5000"
+      process.env.NEXT_PUBLIC_API_URL!
+);
 
 export default socket;
